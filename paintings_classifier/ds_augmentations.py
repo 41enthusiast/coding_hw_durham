b5535113d5cc427b594385f2d5d39f5770c47349
41enthusiast <txlx81@durham.ac.uk>
2022-03-31 08:09:14 +0100

    augmentation for the dataset

---
+++ paintings_classifier/ds_augmentations.py
@@ -0,0 +1,61 @@
+import torchvision.transforms.functional as tvf
+from torchvision import transforms
+import torch
+from torch.utils.data import Dataset, DataLoader, SubsetRandomSampler, SequentialSampler
+import numpy as np
+import random
+
+class AugDatasetWrapper(Dataset):
+    def __init__(self, ds: Dataset, target_size=(96, 96), debug=False):
+        super().__init__()
+        self.ds = ds
+        self.debug = debug
+        self.target_size = target_size
+        if debug:
+            print("DATASET IN DEBUG MODE")
+        
+        # I will be using network pre-trained on ImageNet first, which uses this normalization.
+        # Remove this, if you're training from scratch or apply different transformations accordingly
+        self.preprocess = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
+        
+        random_resized_rotation = WrapWithRandomParams(lambda angle: ResizedRotation(angle, target_size), [(0.0, 360.0)])
+        self.randomize = transforms.Compose([
+            transforms.RandomResizedCrop(target_size, scale=(1/3, 1.0), ratio=(0.3, 2.0)),
+            transforms.RandomChoice([
+                transforms.RandomHorizontalFlip(p=0.5),
+                transforms.Lambda(random_rotate)
+            ]),
+            transforms.RandomApply([
+                random_resized_rotation
+            ], p=0.33),
+            transforms.RandomApply([
+                transforms.ColorJitter(brightness=0.5, contrast=0.5, saturation=0.5, hue=0.2)
+            ], p=0.8),
+            transforms.RandomGrayscale(p=0.2)
+        ])
+    
+    def __len__(self): return len(self.ds)
+    
+    def __getitem_internal__(self, idx, preprocess=True):
+        this_image_raw, ds_cls = self.ds[idx]
+        
+        if self.debug:
+            random.seed(idx)
+            t = self.randomize(this_image_raw)  
+        else:
+            t1 = self.randomize(this_image_raw)
+        if preprocess:
+            t = self.preprocess(t)
+        else:
+            t = transforms.ToTensor()(t)
+
+        return t, torch.tensor(ds_cls)
+
+    def __getitem__(self, idx):
+        return self.__getitem_internal__(idx, True)
+    
+    def raw(self, idx):
+        return self.__getitem_internal__(idx, False)
